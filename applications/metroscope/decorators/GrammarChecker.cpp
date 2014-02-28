6551f03d9bfcab501065ba3d211c1e5becca9bf5
Luis P <luis.prieto@epfl.ch>
2014-02-28 14:57:24 +0100

    Changed the response feedback to a filled rectangle (now that the markers are protected)

--- applications/metroscope/decorators/GrammarChecker.cpp
+++ applications/metroscope/decorators/GrammarChecker.cpp
@@ -103,16 +103,16 @@ void decorators::GrammarChecker::displayMessageHint(std::string pMessage, bool p
 	if(pMessage.length()>0){
 		mDecoratorManager.GetDisplay().PushTransformation();
 		//display the color bar indicating if it is correct or not
-		/*mDecoratorManager.GetDisplay().RenderQuadFilled(0.0f, sentenceCenter.y+scGUIDELINE_DISTANCE,
+		mDecoratorManager.GetDisplay().RenderQuadFilled(0.0f, sentenceCenter.y+scGUIDELINE_DISTANCE,
 				mDecoratorManager.GetDisplay().GetWidth(), sentenceCenter.y+scGUIDELINE_DISTANCE,
 				mDecoratorManager.GetDisplay().GetWidth(), sentenceCenter.y-scGUIDELINE_DISTANCE,
 				0.0f, sentenceCenter.y-scGUIDELINE_DISTANCE,
-				scGREEN.r, scGREEN.g, scGREEN.b, 0.3f);*/
-		mDecoratorManager.GetDisplay().RenderQuad(0.0f, sentenceCenter.y+scGUIDELINE_DISTANCE,
+				scGREEN.r, scGREEN.g, scGREEN.b, 0.3f);
+		/*mDecoratorManager.GetDisplay().RenderQuad(0.0f, sentenceCenter.y+scGUIDELINE_DISTANCE,
 				mDecoratorManager.GetDisplay().GetWidth(), sentenceCenter.y+scGUIDELINE_DISTANCE,
 				mDecoratorManager.GetDisplay().GetWidth(), sentenceCenter.y-scGUIDELINE_DISTANCE,
 				0.0f, sentenceCenter.y-scGUIDELINE_DISTANCE,
-				scGREEN.r, scGREEN.g, scGREEN.b, 1.0f);
+				scGREEN.r, scGREEN.g, scGREEN.b, 1.0f);*/
 
 		//display the message or hint - GET RID OF MAGIC NUMBERS!
 		mDecoratorManager.GetDisplay().RenderCenteredTextFixedWidth(pMessage.c_str(), scTEXT_DELIMITERS,
