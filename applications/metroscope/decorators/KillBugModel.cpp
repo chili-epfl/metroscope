f4ef7b709ffe3eefb512ef1a123f0395f3c76efd
Daniela <daniela.caballerodiaz@epfl.ch>
2014-06-03 19:11:50 +0200

    Adding textures

--- applications/metroscope/decorators/KillBugModel.cpp
+++ applications/metroscope/decorators/KillBugModel.cpp
@@ -259,7 +259,7 @@ void decorators::KillBugModel::DisplayMap(){
 	}
 
 	//Obstacles points
-	if(mActualCarte->getObstacleNumber() > 0){
+	/*if(mActualCarte->getObstacleNumber() > 0){
 			std::vector<wykobi::point2d<float>> tObstacle = mActualCarte->getObstaclesPoint();
 
 		for(std::vector<wykobi::point2d<float>>::iterator it = tObstacle.begin(); it != tObstacle.end() ; ++it){
@@ -267,7 +267,20 @@ void decorators::KillBugModel::DisplayMap(){
 			float tEndPositionY = mMapPoint1.y + (*it).y*(mCellDimensionY);
 			mDecoratorManager.GetDisplay().RenderQuadFilled(tEndPositionX,tEndPositionY,tEndPositionX + mCellDimensionX,tEndPositionY,tEndPositionX+mCellDimensionX, tEndPositionY + mCellDimensionY, tEndPositionX, tEndPositionY + mCellDimensionY,0.25f,0.25f,0.25f,1.0f);
 		}
+	}*/
+
+	int tObstacleTextureId = mDecoratorManager.GetDisplay().LoadTexture("./activities/proportions-network/obstacle-small.jpg");
+	std::vector<wykobi::point2d<float>> tObstacle = mActualCarte->getObstaclesPoint();
+
+
+	for(std::vector<wykobi::point2d<float>>::iterator it = tObstacle.begin(); it != tObstacle.end() ; ++it){
+		mDecoratorManager.GetDisplay().RenderTexture(tObstacleTextureId,
+				mMapPoint1.x + (*it).x*mCellDimensionX,mMapPoint1.y + (*it).y*mCellDimensionY,
+				mMapPoint1.x + (*it).x*mCellDimensionX + mCellDimensionX,mMapPoint1.y + (*it).y*mCellDimensionY,
+				mMapPoint1.x + (*it).x*mCellDimensionX + mCellDimensionX,mMapPoint1.y + (*it).y*mCellDimensionY + mCellDimensionY,
+				mMapPoint1.x + (*it).x*mCellDimensionX,mMapPoint1.y + (*it).y*mCellDimensionY + mCellDimensionY);
 	}
+
 
 	if(mMapFinished){
 		mDecoratorManager.GetDisplay().RenderQuadFilled(mMapPoint1.x, mDisplayHeight/2 - mWorkingTriangle/4,
@@ -369,9 +382,20 @@ void decorators::KillBugModel::DisplayBug(){
 	float tBugPositionX = mMapPoint1.x + (mBugPosition.x)*(mCellDimensionX)+(mCellDimensionX/2);
 	float tBugPositionY = mMapPoint1.y + (mBugPosition.y)*(mCellDimensionY)+(mCellDimensionY/2);
 
+	int tTextureId = mDecoratorManager.GetDisplay().LoadTexture("./activities/proportions-network/ladybug-smallwhite.jpg");
+
+	//Display the image
 	mDecoratorManager.GetDisplay().PushTransformation();
-	mDecoratorManager.GetDisplay().RenderFilledEllipse(tBugPositionX, tBugPositionY, mCellDimensionX/6,mCellDimensionY/6,0.0f,1.0f,0.0f,1.0f,1);
+		mDecoratorManager.GetDisplay().RenderTexture(tTextureId,
+				mMapPoint1.x + mBugPosition.x*mCellDimensionX,mMapPoint1.y + mBugPosition.y*mCellDimensionY,
+				mMapPoint1.x + mBugPosition.x*mCellDimensionX + mCellDimensionX,mMapPoint1.y + mBugPosition.y*mCellDimensionY,
+				mMapPoint1.x + mBugPosition.x*mCellDimensionX + mCellDimensionX,mMapPoint1.y + mBugPosition.y*mCellDimensionY + mCellDimensionY,
+				mMapPoint1.x + mBugPosition.x*mCellDimensionX,mMapPoint1.y + mBugPosition.y*mCellDimensionY + mCellDimensionY);
 	mDecoratorManager.GetDisplay().PopTransformation();
+
+	//mDecoratorManager.GetDisplay().PushTransformation();
+	//mDecoratorManager.GetDisplay().RenderFilledEllipse(tBugPositionX, tBugPositionY, mCellDimensionX/6,mCellDimensionY/6,0.0f,1.0f,0.0f,1.0f,1);
+	//mDecoratorManager.GetDisplay().PopTransformation();
 }
 
 void decorators::KillBugModel::FetchProportions(){
