937e01b4e0843b0e03d0aca76ec219bcf31653cb
Daniela <daniela.caballerodiaz@epfl.ch>
2014-06-03 13:06:44 +0200

    Fixing inverse bug

--- applications/metroscope/decorators/CircularFractionModel.cpp
+++ applications/metroscope/decorators/CircularFractionModel.cpp
@@ -67,7 +67,7 @@ void decorators::CircularFractionModel::update(){
 
 		float tAngle = wykobi::oriented_vertex_angle(tWorldStartPoint, tWorldOriginPoint, tWorldEndPoint, wykobi::Clockwise);
 
-		mProportion = tAngle/360.0f;
+		mProportion = (1 - tAngle/360.0f);
 		mDenominator = 10;
 		mNumerator = ((int)(mProportion*100)%10 >= 5) ? ceil((mProportion*100)/10) : floor((mProportion*100)/10);
 	}
