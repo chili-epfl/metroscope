6be8125ddef86131a12a570a04ff44ed4a09f3ed
Daniela <daniela.caballerodiaz@epfl.ch>
2014-06-17 10:24:44 +0200

    Calculating fractions in rectangle model

--- applications/metroscope/decorators/RectangleFractionModel.hpp
+++ applications/metroscope/decorators/RectangleFractionModel.hpp
@@ -33,6 +33,7 @@ class RectangleFractionModel : public FiducialDecorator{
 		bool isPresent();
 		const wykobi::point2d<float> &StartPoint() const {return mStartPoint;}
 		const wykobi::point2d<float> &EndPoint() const {return mEndPoint;}
+		void CalculateFractionFromDecimal();
 		float proportion() const {return mProportion;}
 		int Numerator() const {return mNumerator;}
 		int Denominator() const {return mDenominator;}
